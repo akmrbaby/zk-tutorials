242f265d736fdd915231ce97f43eb8f42028d512
akmrbaby <61498911+akmrbaby@users.noreply.github.com>
2025-09-06 14:52:54 +0900

    Refactor comments for clarity in generateProof.ts

--- zkemail/generateProof.ts
+++ zkemail/generateProof.ts
@@ -9,7 +9,7 @@ async function main() {
   const blueprint = await sdk.getBlueprint(blueprintSlug);
   const prover = blueprint.createProver();
 
-  // サンプル用の eml を取得
+  // サンプルメールを取得
   const eml = (await fs.readFile("./sample/residency.eml")).toString();
 
   // 証明を生成
@@ -19,7 +19,7 @@ async function main() {
   console.log("proof: ", proofData);
   console.log("public: ", publicOutputs);
 
-  // 証明と公開アウトプットを保存
+  // 保存
   await fs.writeFile("proofData.json", JSON.stringify(proofData));
   await fs.writeFile("publicOutputs.json", JSON.stringify(publicOutputs));
   console.log("Saved: proofData.json & publicOutputs.json");
