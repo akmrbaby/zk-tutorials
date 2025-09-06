003787945296f7d4311caeaa607e9bcfb2d38fd6
akmrbaby <61498911+akmrbaby@users.noreply.github.com>
2025-09-06 14:50:37 +0900

    Implement proof generation and verification functionality; add proofData.json and publicOutputs.json files

--- zkemail/generateProof.ts
+++ zkemail/generateProof.ts
@@ -1,23 +1,28 @@
 import zkeSDK from "@zk-email/sdk";
 import fs from "fs/promises";
 
-const blueprintSlug = "DimiDumo/SuccinctZKResidencyInvite@v3";
-
 async function main() {
   const sdk = zkeSDK();
 
   // Blueprint（＝ゼロ知識証明の回路の定義）のインスタンスを取得
+  const blueprintSlug = "DimiDumo/SuccinctZKResidencyInvite@v3";
   const blueprint = await sdk.getBlueprint(blueprintSlug);
-  const prover = blueprint.createProver({ isLocal: false }); // blueprint が prover？
+  const prover = blueprint.createProver();
 
   // サンプル用の eml を取得
   const eml = (await fs.readFile("./sample/residency.eml")).toString();
 
   // 証明を生成
   const proof = await prover.generateProof(eml);
-  const { proofData, publicData } = proof.getProofData();
+  const proofData = proof.props.proofData;
+  const publicOutputs = proof.props.publicOutputs;
   console.log("proof: ", proofData);
-  console.log("public: ", publicData);
+  console.log("public: ", publicOutputs);
+
+  // 証明と公開アウトプットを保存
+  await fs.writeFile("proofData.json", JSON.stringify(proofData));
+  await fs.writeFile("publicOutputs.json", JSON.stringify(publicOutputs));
+  console.log("Saved: proofData.json & publicOutputs.json");
 }
 
 main();
---
+++ zkemail/verifyProof.ts
@@ -0,0 +1,23 @@
+import zkeSDK from "@zk-email/sdk";
+import fs from "fs/promises";
+
+async function main() {
+  const sdk = zkeSDK();
+
+  // Blueprint（＝ゼロ知識証明の回路の定義）のインスタンスを取得
+  const blueprintSlug = "DimiDumo/SuccinctZKResidencyInvite@v3";
+  const blueprint = await sdk.getBlueprint(blueprintSlug);
+
+  // 保存済みファイルをロード
+  const proofDataJSON = await fs.readFile("./proofData.json", "utf-8");
+  const publicOutputsJSON = await fs.readFile("./publicOutputs.json", "utf-8");
+
+  // SDK に渡して検証
+  const isValid = await blueprint.verifyProofData(
+    publicOutputsJSON,
+    proofDataJSON
+  );
+  console.log("isValid:", isValid);
+}
+
+main();
